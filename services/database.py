04267f128373cfa312b4925cb026debd3822ae0b
Edvaldo Gutierres <80729345+edvaldo-gutierres@users.noreply.github.com>
2023-11-13 19:27:52 -0300

    Update database.py

--- services/database.py
+++ services/database.py
@@ -2,18 +2,11 @@
 import mysql.connector
 
 # Conectar ao banco de dados MySQL
-# conn = mysql.connector.connect(
-#     host='127.0.0.1',
-#     port=3306,
-#     user='root',
-#     password='1234',
-#     database='db_projetoelas'
-# )
-
 conn = mysql.connector.connect(
-    host='containers-us-west-46.railway.app',
-    port=6409,
-    user='root',
-    password='wMzbX6qodmA8SWLDH8j0',
-    database='db_projetoelas'
-)
+     host='127.0.0.1',
+     port=3306,
+     user='root',
+     password='1234',
+     database='db_projetoelas'
+ )
+
